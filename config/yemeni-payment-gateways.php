e0329ecc5d33d89fc50346e6e9b50b5f9be7778b
aymanalhattami <ayman.m.alhattami@gmail.com>
2024-08-12 14:25:58 +0300

    refactor: config file

--- config/yemeni-payment-gateways.php
+++ config/yemeni-payment-gateways.php
@@ -1,14 +1,11 @@
 <?php
 
-/*
- * You can place your custom package configuration in here.
- */
 return [
     'floosak' => [
-        'base_url' => env('FLOOSAK_BASE_URL', null),
-        'phone' => env('FLOOSAK_PHONE', null),
-        'short_code' => env('FLOOSAK_SHORT_CODE', null),
-        'wallet_id' => env('FLOOSAK_WALLET_ID', null),
-        'key' => env('FLOOSAK_KEY', null),
+        'base_url' => env('FLOOSAK_BASE_URL'),
+        'phone' => env('FLOOSAK_PHONE'),
+        'short_code' => env('FLOOSAK_SHORT_CODE'),
+        'wallet_id' => env('FLOOSAK_WALLET_ID'),
+        'key' => env('FLOOSAK_KEY'),
     ]
 ];
