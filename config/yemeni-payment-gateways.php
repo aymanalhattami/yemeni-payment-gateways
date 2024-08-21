277d080538450b7532e143a4c57cb761dfb4d75e
aymanalhattami <ayman.m.alhattami@gmail.com>
2024-08-21 09:53:09 +0300

    wip: jawali

--- config/yemeni-payment-gateways.php
+++ config/yemeni-payment-gateways.php
@@ -15,8 +15,8 @@ return [
         'login_token' => env('JAWALI_LOGIN_TOKEN'),
         'access_token' => env('JAWALI_ACCESS_TOKEN'),
         'org_id' => env('JAWALI_ORG_ID'),
-        'user_id' => env('JAWALI_USER_ID'),
-        'user_pwd' => env('JAWALI_USER_PWD'),
+        'username' => env('JAWALI_USERNAME'),
+        'password' => env('JAWALI_PASSWORD'),
         'agent_wallet' => env('JAWALI_AGENT_WALLET'),
         'agent_wallet_pwd' => env('JAWALI_AGENT_WALLET_PWD')
     ],
