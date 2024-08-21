c67f46d79b048937a31606e28d45791f7182e1b8
aymanalhattami <ayman.m.alhattami@gmail.com>
2024-08-21 09:47:01 +0300

    wip: jawali

--- config/yemeni-payment-gateways.php
+++ config/yemeni-payment-gateways.php
@@ -8,5 +8,16 @@ return [
         'wallet_id' => env('FLOOSAK_WALLET_ID'),
         'key' => env('FLOOSAK_KEY'),
         'verify_request_id' => env('FLOOSAK_VERIFY_REQUEST_ID')
-    ]
+    ],
+
+    'jawali' => [
+        'base_url' => env('JAWALI_BASE_URL'),
+        'login_token' => env('JAWALI_LOGIN_TOKEN'),
+        'access_token' => env('JAWALI_ACCESS_TOKEN'),
+        'org_id' => env('JAWALI_ORG_ID'),
+        'user_id' => env('JAWALI_USER_ID'),
+        'user_pwd' => env('JAWALI_USER_PWD'),
+        'agent_wallet' => env('JAWALI_AGENT_WALLET'),
+        'agent_wallet_pwd' => env('JAWALI_AGENT_WALLET_PWD')
+    ],
 ];
---
+++ src/Jawali/Jawali.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Aymanalhattami\YemeniPaymentGateways\Jawali;
+
+use Aymanalhattami\YemeniPaymentGateways\PaymentGateway;
+use Aymanalhattami\YemeniPaymentGateways\Status;
+use Exception;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Support\Facades\Http;
+
+class Jawali extends PaymentGateway
+{
+    private function getBaseUrl(): string
+    {
+        $url = rtrim(config('yemeni-payment-gateways.jawali.base_url'), '/');
+
+        return $url . '/';
+    }
+
+
+    public function login(): static
+    {
+        try {
+            $this->response = Http::asForm()->post($this->getBaseUrl() . 'paygate/oauth/token', [
+                'username' => $this->getUsername(),
+                'password' => $this->getPassword(),
+                'grant_type' => 'password',
+                'client_id' => 'restapp',
+                'client_secret' => 'restapp',
+                'scope' => 'read',
+            ]);
+
+            if ($this->response->failed()) {
+                throw new Exception($this->response->object()->error_description);
+            } else {
+                $this->unifiedResponse
+                    ->status(Status::Success->value)
+                    ->success(true)
+                    ->data($this->response->json());
+            }
+        } catch (Exception $e) {
+            $this->unifiedResponse
+                ->status(Status::Failed->value)
+                ->success(false)
+                ->message($e->getMessage())
+                ->data($this->response->json());
+        }
+
+
+        return $this;
+    }
+
+    private function getUsername(): string
+    {
+        return config('yemeni-payment-gateways.jawali.username');
+    }
+
+    private function getPassword(): string
+    {
+        return config('yemeni-payment-gateways.jawali.password');
+    }
+}
